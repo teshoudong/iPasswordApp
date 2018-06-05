34d9ec24ea9ab98e3be8123a44f87e13ce73d8c3
董健华 <dongjianhua@corp.netease.com>
2018-06-05 10:02:26 +0800

    添加退出登录

--- src/setting.js
+++ src/setting.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Setting extends React.Component {
+    handleLogout() {
+        
+    }
+
     render() {
         return (
-            <Text>
-                Setting
-            </Text>
+            <ScrollView>
+                <View style={styles.logout}>
+                    <TouchableOpacity 
+                        style={styles.logoutButton} 
+                        activeOpacity={0.9} 
+                        onPress={() => this.handleLogout()}>
+                        <Text style={styles.logoutButtonText}>退出登录</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    logout: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginTop: 10
+    },
+    logoutButton: {
+        backgroundColor: '#DA3610',
+        height: 40,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logoutButtonText: {
+        fontSize: 12,
+        color: '#FFFFFF'
+    } 
+});
